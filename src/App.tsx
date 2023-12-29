92d9ee3992b25a97a271ef6b31bba78332a21bbd
Jovan Medarovic <end54065@adobe.com>
2023-12-29 23:36:41 +0100

    Removed ClickCounter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import ClickCounter from './ClickCounter';
 import './style.scss';
 
 const App = () => {
 	return (
 		<>
 			<h1>Page Content Title</h1>
-			<ClickCounter />
 		</>
 	);
 };
